af2db441ae5f3b6674123e2c5e98b6743bbb37ba
Lucasasdev <Lucasasdev@gmail.com>
2024-01-06 13:34:17 -0300

    initial commit

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,10 +4,8 @@ require("dotenv").config();
 const connectionToDatabase = async () => {
   try {
     await mongoose
-      .connect(
-        `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.culbnn3.mongodb.net/UserData?retryWrites=true&w=majority`
-      )
-      .then(console.log("Connected to database!"));
+      .connect(process.env.MONGODB_CONNECT_URI)
+      .then(() => console.log("Connected to database!"));
   } catch (error) {
     console.log(error);
   }
